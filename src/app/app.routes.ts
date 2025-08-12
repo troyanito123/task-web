e12716dd29285b4da3a8d3b0452672091b3308d6
Jass <jassmanniq@gmail.com>
2025-08-12 18:52:58 -0400

    add main pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./authentication/presentation/login/login'),
+  },
+  {
+    path: 'tasks',
+    loadComponent: () => import('./task/presentation/task-list/task-list'),
+  },
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./shared/not-found/not-found'),
+  },
+];
